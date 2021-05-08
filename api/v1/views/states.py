426a4500a9d7c7bcd165b02d04616db8de3dc500
sebastian <sebastian.calleu@gmail.com>
2021-05-07 22:58:16 -0500

    refactor(structure): making an individual method for each request method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,10 +7,68 @@ from models import storage
 from models.state import State
 
 
+@app_views.route('/states', methods=["GET"], strict_slashes=False)
+def statesgetall():
+    dct1 = storage.all(State)
+    lt1 = []
+    for i in dct1.values():
+        lt1.append(i.to_dict())
+    return jsonify(lt1)
+
+
+@app_views.route('/states/<state_id>', methods=["GET"], strict_slashes=False)
+def stateget(state_id=None):
+    try:
+        obj1 = storage.get(State, state_id)
+    except:
+        abort(404)
+    return jsonify(obj1.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=["DELETE"], strict_slashes=False)
+def statedel(state_id=None):
+    try:
+        obj1 = storage.get(State, state_id)
+    except:
+        abort(404)
+    storage.delete(obj1)
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=["POST"], strict_slashes=False)
+def statepost():
+    try:
+        jsondata = request.get_json()
+    except:
+        abort(400, description="Not a JSON")
+    if not jsondata["name"]:
+        abort(400, description="Missing name")
+    newobj = State(**jsondata)
+    newobj.save()
+    return make_response(jsonify(newobj.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=["PUT"], strict_slashes=False)
+def stateput():
+    try:
+        jsonupdt = request.get_json()
+    except:
+        abort(400, description="Not a JSON")
+    try:
+        obj1 = storage.get(State, state_id)
+    except:
+        abort(404)
+    for i, j in jsonupdt.items():
+        if i != "id" and i != "created_at" and i != "updated_at":
+            setattr(obj1, i, j)
+    obj1.save()
+    return make_response(jsonify(obj1.to_dict()), 200)
+
+"""
 @app_views.route('/states', methods=["GET", "POST"], strict_slashes=False)
 @app_views.route('/states/<state_id>', methods=["GET", "DELETE", "PUT"], strict_slashes=False)
 def states(state_id=None):
-    """ method to manipulate states object """
+    ´´´ method to manipulate states object ´´´
     if request.method == "GET":
         if state_id is None:
             dct1 = storage.all(State)
@@ -55,3 +113,4 @@ def states(state_id=None):
                 setattr(obj1, i, j)
         obj1.save()
         return make_response(jsonify(obj1.to_dict()), 200)
+"""
