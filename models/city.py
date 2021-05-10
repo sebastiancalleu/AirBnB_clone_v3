d3abc5686029363d021b8bc9ffd1e1c642c94b3a
sebastian <sebastian.calleu@gmail.com>
2021-05-10 11:02:54 -0500

    feat(places): adding property places

--- models/city.py
+++ models/city.py
@@ -15,6 +15,18 @@ class City(BaseModel, Base):
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship("Place", backref="cities")
+
+    @property
+    def places(self):
+        """getter attribute returns the list of Place instances"""
+        from models.place import Place
+        place_list = []
+        all_places = models.storage.all(Place)
+        for place in all_places.values():
+            if place.city_id == self.id:
+               place_list.append(place)
+        return place_list
+
     else:
         state_id = ""
         name = ""
